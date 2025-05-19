83c121c624f9c2eeaac862818efb1708f0a964ed
wyr <18026086011@163.com>
2025-05-19 22:20:42 +0800

    feat: commit校验测试

---
+++ commitlint.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']], // 允许的提交类型
+    'subject-case': [0] // 不限制 subject 大小写
+  }
+}
