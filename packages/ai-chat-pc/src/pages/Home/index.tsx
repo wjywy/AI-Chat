e4d273caae699ea85a15afb37cbdac7caddadf80
wyr <18026086011@163.com>
2025-05-22 08:06:34 +0800

    feat: 多模态输入框(pnpm版本变动导致锁文件diff较大，可忽略)

--- packages/ai-chat-pc/src/pages/Home/index.tsx
+++ packages/ai-chat-pc/src/pages/Home/index.tsx
@@ -1,26 +1,5 @@
-import { Bubble, Sender } from '@ant-design/x'
-import useBearStore from '@pc/store/useBearStore'
-
-const messages = [
-  {
-    content: 'Hello, Ant Design X!',
-    role: 'user'
-  }
-]
-
 const Home = () => {
-  const bears = useBearStore((state) => state.bears)
-  const name = 'zs'
-
-  return (
-    <>
-      <p className="font-bold">this is Home - {bears}</p>
-      <div>
-        <Bubble.List items={messages} />
-        <Sender />
-      </div>
-    </>
-  )
+  return <></>
 }
 
 export default Home
